24c97123fd06bd263cf429a3c52a5f2419d1defc
Elnar Mengelbaev <elnar_men@mail.ru>
2023-09-07 16:29:54 +0300

    add .gitignore

--- reader.py
+++ reader.py
@@ -1,11 +1,12 @@
 import asyncio
 
+
 async def tcp_echo_client():
     reader, writer = await asyncio.open_connection(
         'minechat.dvmn.org', 5000)
     while True:
         data = await reader.read(200)
-        print(f'{data.decode()!r}')
+        print(data.decode().strip())
 
 
 asyncio.run(tcp_echo_client())
