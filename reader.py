0f11a959bcdd643f50eef43b9d343b5efd7dea5b
Elnar Mengelbaev <elnar_men@mail.ru>
2023-09-22 14:45:00 +0300

    Save messages history to file

--- reader.py
+++ reader.py
@@ -4,14 +4,28 @@ from datetime import datetime
 
 
 async def tcp_echo_client():
-    reader, writer = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000)
     while True:
-        now = datetime.now().strftime("%d.%m.%y %H:%M")
-        data = await reader.read(200)
-        formatted_data = f'[{now}] {data.decode()}'
-        async with aiofiles.open('output.txt', 'a') as file:
-            await file.write(formatted_data)
-        print(formatted_data.strip())
+        try:
+            reader, writer = await asyncio.open_connection(
+                'minechat.dvmn.org', 5000)
+            try:
+                while True:
+                    current_datetime = datetime.now()
+                    current_datetime_str = current_datetime.strftime("%d.%m.%y %H:%M")
+                    data = await reader.read(200)
+                    if not data:
+                        raise ConnectionError("Connection lost")
+                    formatted_data = \
+                        f'[{current_datetime_str}] {data.decode("utf-8", "ignore")}'
+                    async with aiofiles.open('output.txt', 'a') as file:
+                        await file.write(formatted_data)
+                    print(formatted_data.strip())
+            finally:
+                writer.close()
+                await writer.wait_closed()
+        except ConnectionError:
+            print(f'[{datetime.now().strftime("%d.%m.%y %H:%M")}] Connection lost. Reconnecting in 5 seconds...')
+            await asyncio.sleep(5)
+
 
 asyncio.run(tcp_echo_client())
