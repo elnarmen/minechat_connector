97e8310db62786e729b7d201e3fddc944ae8ad2b
Elnar Mengelbaev <elnar_men@mail.ru>
2023-09-25 12:00:07 +0300

    setup logging

--- writer.py
+++ writer.py
@@ -1,4 +1,11 @@
 import asyncio
+import logging
+
+
+FORMAT = '%(levelname)s:%(message)s'
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+logging.getLogger('asyncio').setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
 
 
 async def tcp_echo_client(message, account_hash='da751b26-5b73-11ee-aae7-0242ac110002'):
@@ -6,21 +13,29 @@ async def tcp_echo_client(message, account_hash='da751b26-5b73-11ee-aae7-0242ac1
         'minechat.dvmn.org', 5050)
 
     greeting = await reader.readline()
-    print(f'Received: {greeting.decode().strip()}')
+    logger.debug(f'Response: {greeting.decode().strip()}')
 
-    writer.write(f"{account_hash}\n".encode())
+    writer.write(f'{account_hash}\n'.encode())
+    logger.debug(f'Message: {account_hash.strip()}')
     await writer.drain()
 
     response = await reader.readline()
-    print(f'Received: {response.decode().strip()}')
-    response = await reader.readline()
-    print(f'Received: {response.decode().strip()}')
+    logger.debug(f'Response: {response.decode().strip()}')
 
-    writer.write(f"{message}\n\n".encode())
+    response = await reader.readline()
+    logger.debug(f'Response: {response.decode().strip()}')
+
+    writer.write(f'{message}\n'.encode())
+    logger.debug(f'Message: {message}')
     await writer.drain()
 
     writer.close()
     await writer.wait_closed()
 
 
-asyncio.run(tcp_echo_client('Test message'))
+def main():
+    asyncio.run(tcp_echo_client('Test message'))
+
+
+if __name__ == '__main__':
+    main()
