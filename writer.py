1a9a78935c1bd3776f3ccc034a034c6a1da7dd20
Elnar Mengelbaev <elnar_men@mail.ru>
2023-09-25 14:05:12 +0300

    add check for broken token

--- writer.py
+++ writer.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import logging
 
 
@@ -8,25 +9,29 @@ logging.getLogger('asyncio').setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
-async def tcp_echo_client(message, account_hash='da751b26-5b73-11ee-aae7-0242ac110002'):
+async def tcp_echo_client(message, token='da751b26-5b73-11ee-aae7-0242ac110002'):
     reader, writer = await asyncio.open_connection(
         'minechat.dvmn.org', 5050)
 
     greeting = await reader.readline()
-    logger.debug(f'Response: {greeting.decode().strip()}')
+    logger.debug(greeting.decode().strip())
 
-    writer.write(f'{account_hash}\n'.encode())
-    logger.debug(f'Message: {account_hash.strip()}')
+    writer.write(f'{token}\n'.encode())
+    logger.debug(f'Отправлен токен: {token}')
     await writer.drain()
 
-    response = await reader.readline()
-    logger.debug(f'Response: {response.decode().strip()}')
+    auth_result = json.loads(await reader.readline())
+    if not auth_result:
+        logger.debug(f'Ответ: Неизвестный токен. Проверьте его или зарегистрируйте заново.')
+        return
+
+    logger.debug(f'Ответ: {auth_result}')
 
     response = await reader.readline()
-    logger.debug(f'Response: {response.decode().strip()}')
+    logger.debug(f'Ответ: {response.decode().strip()}')
 
-    writer.write(f'{message}\n'.encode())
-    logger.debug(f'Message: {message}')
+    writer.write(f'{message}\n\n'.encode())
+    logger.debug(f'Сообщение: {message}')
     await writer.drain()
 
     writer.close()
