ddcb75f425c20779267e23808758a29cccdec548
Elnar Mengelbaev <elnar_men@mail.ru>
2023-09-30 15:28:35 +0300

    add a context manager to close the connection

--- writer.py
+++ writer.py
@@ -4,6 +4,7 @@ import json
 import logging
 import os
 import aiofiles
+import contextlib
 
 from dotenv import load_dotenv
 
@@ -14,35 +15,44 @@ logging.getLogger('asyncio').setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
-async def register(username, host, port, message):
+@contextlib.asynccontextmanager
+async def manage_tcp_connection(host, port):
     reader, writer = await asyncio.open_connection(host, port)
+    try:
+        yield reader, writer
+    finally:
+        writer.close()
+        await writer.wait_closed()
 
-    response = await reader.readline()
-    logger.debug(f'Ответ: {response.decode().strip()}')
 
-    writer.write('\n'.encode())
-    logger.debug('Отправлено пустое сообщение для перехода в регистрацию')
-    await writer.drain()
+async def register(username, host, port, message):
+    async with manage_tcp_connection(host, port) as (reader, writer):
+        response = await reader.readline()
+        logger.debug(f'Ответ: {response.decode().strip()}')
 
-    response = await reader.readline()
-    logger.debug(f'Ответ: {response.decode().strip()}')
+        writer.write('\n'.encode())
+        logger.debug('Отправлено пустое сообщение для перехода в регистрацию')
+        await writer.drain()
 
-    username = username.replace('\n', '').strip()
-    writer.write(f'{username}\n'.encode())
-    logger.debug(f'Отправлен никнейм {username} для регистрации')
-    await writer.drain()
+        response = await reader.readline()
+        logger.debug(f'Ответ: {response.decode().strip()}')
 
-    registration_result = json.loads(await reader.readline())
-    token = registration_result['account_hash']
-    async with aiofiles.open('.env', 'a') as file:
-        await file.write(f'USER_TOKEN={token}')
-    logger.debug(f'Ответ: {registration_result}')
-    logger.debug('Токен сохранен в файле ".env"')
+        username = username.replace('\n', '').strip()
+        writer.write(f'{username}\n'.encode())
+        logger.debug(f'Отправлен никнейм {username} для регистрации')
+        await writer.drain()
 
-    writer.close()
-    await writer.wait_closed()
+        registration_result = json.loads(await reader.readline())
+        token = registration_result['account_hash']
+        async with aiofiles.open('.env', 'a') as file:
+            await file.write(f'USER_TOKEN={token}')
+        logger.debug(f'Ответ: {registration_result}')
+        logger.debug('Токен сохранен в файле ".env"')
 
-    await tcp_client(message, token, host, port)
+        writer.close()
+        await writer.wait_closed()
+
+        await tcp_client(message, token, host, port)
 
 
 async def submit_message(writer, message):
@@ -53,28 +63,24 @@ async def submit_message(writer, message):
 
 
 async def tcp_client(message, token, host, port):
-    reader, writer = await asyncio.open_connection(host, port)
+    async with manage_tcp_connection(host, port) as (reader, writer):
+        greeting = await reader.readline()
+        logger.debug(greeting.decode().strip())
 
-    greeting = await reader.readline()
-    logger.debug(greeting.decode().strip())
+        writer.write(f'{token}\n'.encode())
+        logger.debug(f'Отправлен токен: {token}')
+        await writer.drain()
 
-    writer.write(f'{token}\n'.encode())
-    logger.debug(f'Отправлен токен: {token}')
-    await writer.drain()
+        auth_result = json.loads(await reader.readline())
+        if not auth_result:
+            logger.debug(f'Ответ: Неизвестный токен. Проверьте его или зарегистрируйте заново.')
+            return
 
-    auth_result = json.loads(await reader.readline())
-    if not auth_result:
-        logger.debug(f'Ответ: Неизвестный токен. Проверьте его или зарегистрируйте заново.')
-        return
+        logger.debug(f'Ответ: {auth_result}')
+        response = await reader.readline()
+        logger.debug(f'Ответ: {response.decode().strip()}')
 
-    logger.debug(f'Ответ: {auth_result}')
-    response = await reader.readline()
-    logger.debug(f'Ответ: {response.decode().strip()}')
-
-    await submit_message(writer, message)
-
-    writer.close()
-    await writer.wait_closed()
+        await submit_message(writer, message)
 
 
 def main():
